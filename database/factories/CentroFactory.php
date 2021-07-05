13e7d42d875457fb76e62f62c78e28ec920612b1
Jose Manuel Cassola <jcassola@uclv.cu>
2021-07-05 17:45:53 -0400

    add migrations, model and factories for tables

---
+++ database/factories/CentroFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Centro;
+use Faker\Generator as Faker;
+
+$factory->define(Centro::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/DirectorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Director;
+use Faker\Generator as Faker;
+
+$factory->define(Director::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
