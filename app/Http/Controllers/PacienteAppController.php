162e7f82d3ddedc22e1b92d9e31ce23095556d32
Jose Manuel Cassola <jcassola@uclv.cu>
2021-07-12 16:00:24 -0400

    add store method for app paciente

--- app/Http/Controllers/PacienteAppController.php
+++ app/Http/Controllers/PacienteAppController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\DatosPaciente;
+use App\Http\Resources\PacienteAppResource;
 use App\PacienteApp;
 use Illuminate\Http\Request;
+use Validator;
 
 class PacienteAppController extends Controller
 {
@@ -25,7 +28,38 @@ class PacienteAppController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        // $validator = Validator::make($data, [
+        //     'id_centro' => 'required',
+        //     'nombre' => 'required|unique:areas',
+        //     'categoria' => 'required|',
+        // ]);
+
+        // if($validator->fails()){
+        //     return response()->json(['error' => $validator->errors(),
+        //                         'message'=> 'Hay datos incorrectos']);
+        // }
+
+        $paciente = DatosPaciente::find($data['id_paciente']);
+
+        $app = new PacienteApp();
+        $app->hipertension = $data['hipertension'];
+        $app->diabetes = $data['diabetes'];
+        $app->asma = $data['asma'];
+        $app->obesidad = $data['obesidad'];
+        $app->insuficiencia_renal = $data['insuficiencia_renal'];
+        $app->embarazo = $data['embarazo'];
+        $app->ninho = $data['ninho'];
+        $app->oncologia = $data['oncologia'];
+        $app->otros = $data['otros'];
+
+        $app->paciente()->associate($paciente)->save();
+
+
+        return response()->json([ 'app' => new PacienteAppResource($app),
+                            'message' => 'Antecedentes patológicos registrados'],
+                            200);
     }
 
     /**
