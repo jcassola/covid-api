d5b4ae953344f6923ff5e63ee0d528e1572989ba
Jose Manuel Cassola <jcassola@uclv.cu>
2021-07-12 15:07:48 -0400

    make relationship get methods for paciente

--- app/Http/Controllers/DatosPacienteController.php
+++ app/Http/Controllers/DatosPacienteController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\DatosPaciente;
 use App\Http\Resources\DatosPacienteResource;
 use App\Http\Resources\DatosPacienteResourceCollection;
+use App\Http\Resources\PacienteAppResource;
+use App\Http\Resources\PacienteContactoResource;
+use App\Http\Resources\PacienteSintomasResource;
 use Illuminate\Http\Request;
 use Validator;
 
@@ -94,6 +97,63 @@ class DatosPacienteController extends Controller
 
         return response()->json([ 'paciente' => new DatosPacienteResource($datosPaciente),
                                 'message' => 'Paciente Eliminado'],
+                                200);
+    }
+
+    /**
+     * Return the apps from specified paciente.
+     *
+     * @param  \App\DatosPaciente  $datosPaciente
+     * @return \Illuminate\Http\Response
+     */
+    public function apps(DatosPaciente $datosPaciente)
+    {
+        $app = $datosPaciente->apps;
+        if(count($app) > 0){
+            return response()->json([ 'apps' => new
+                            PacienteAppResource($app), 'message' => 'Success'],
+                            200);
+        }
+            return response()->json(['message'=>'El paciente no tiene antecedentes patologicos',
+                                'apps'=>null],
+                                200);
+    }
+
+    /**
+     * Return the sintomas from specified paciente.
+     *
+     * @param  \App\DatosPaciente  $datosPaciente
+     * @return \Illuminate\Http\Response
+     */
+    public function sintomas(DatosPaciente $datosPaciente)
+    {
+        $sintomas = $datosPaciente->sintomas;
+        if(count($sintomas) > 0){
+            return response()->json([ 'sintomas' => new
+                            PacienteSintomasResource($sintomas), 'message' => 'Success'],
+                            200);
+        }
+            return response()->json(['message'=>'El paciente no tiene síntomas',
+                                'sintomas'=>null],
+                                200);
+    }
+
+    /**
+     * Return the contactos from specified paciente.
+     *
+     * @param  \App\DatosPaciente  $datosPaciente
+     * @return \Illuminate\Http\Response
+     */
+    public function contactos(DatosPaciente $datosPaciente)
+    {
+        $contactos = $datosPaciente->contactos;
+        if(count($contactos) > 0){
+            return response()->json([ 'contactos' => new
+                            PacienteContactoResource($contactos), 'message' => 'Success'],
+                            200);
+        }
+            return response()->json(['message'=>'El paciente no tiene contactos',
+                                'contactos'=>null],
                                 200);
     }
 }
