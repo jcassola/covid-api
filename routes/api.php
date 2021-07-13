06f5074ce6e8aaedfa85419a9dd805d18cbdadf8
Jose Manuel Cassola <jcassola@uclv.cu>
2021-07-12 21:02:12 -0400

    fix pacientes routes

--- routes/api.php
+++ routes/api.php
@@ -45,9 +45,9 @@ Route::post('/pacientes', 'DatosPacienteController@store');
 Route::get('/pacientes/{datos_paciente}', 'DatosPacienteController@show');
 Route::put('/pacientes/{datos_paciente}', 'DatosPacienteController@update');
 Route::delete('/pacientes/{datos_paciente}', 'DatosPacienteController@destroy');
-Route::get('/pacientes/{datos_paciente}/app', 'HabitacionController@apps');
-Route::get('/pacientes/{datos_paciente}/sintomas', 'HabitacionController@sintomas');
-Route::get('/pacientes/{datos_paciente}/contactos', 'HabitacionController@contactos');
+Route::get('/pacientes/{datos_paciente}/app', 'DatosPacienteController@apps');
+Route::get('/pacientes/{datos_paciente}/sintomas', 'DatosPacienteController@sintomas');
+Route::get('/pacientes/{datos_paciente}/contactos', 'DatosPacienteController@contactos');
 
 
 
